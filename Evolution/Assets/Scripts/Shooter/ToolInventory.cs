094aa0797a15ba1d8d4f203517a5362c43dae3d0
phportal@interia.pl <phportal@interia.pl>
2023-04-15 16:37:13 +0200

    Tool Inventory
    Make Tool Inventory alive

--- Evolution/Assets/Scripts/Shooter/ToolInventory.cs
+++ Evolution/Assets/Scripts/Shooter/ToolInventory.cs
@@ -7,7 +7,7 @@ public class ToolInventory : Inventory
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitializeInventory("Tool");
     }
 
     // Update is called once per frame
